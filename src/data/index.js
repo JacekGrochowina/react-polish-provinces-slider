886a0181cc29c857c7e964b8c15ab212810ef9f9
Jacek Grochowina <jacek99@vp.pl>
2020-04-07 16:23:34 +0200

    import bgImages

--- src/data/index.js
+++ src/data/index.js
@@ -1,115 +1,148 @@
+import zpBg from '../assets/images/zachodniopomorskie.jpg';
+import pmBg from '../assets/images/pomorskie.jpg';
+import wmBg from '../assets/images/warminsko-mazurskie.jpg';
+import pdBg from '../assets/images/podlaskie.jpg';
+import lbBg from '../assets/images/lubuskie.jpg';
+import wpBg from '../assets/images/wielkopolskie.jpg';
+import kpBg from '../assets/images/kujawsko-pomorskie.jpg';
+import ldBg from '../assets/images/lodzkie.jpg';
+import mzBg from '../assets/images/mazowieckie.jpg';
+import luBg from '../assets/images/lubelskie.jpg';
+import dsBg from '../assets/images/dolnoslaskie.jpg';
+import opBg from '../assets/images/opolskie.jpg';
+import slBg from '../assets/images/slaskie.jpg';
+import skBg from '../assets/images/swietokrzyskie.jpg';
+import maBg from '../assets/images/malopolskie.jpg';
+import pkBg from '../assets/images/podkarpackie.jpg';
+
 const DATA = [
     {
         key: 0,
         province: 'Zachodnio-Pomorskie',
         capital: 'Szczecin',
         population: '1 679 000',
-        area: '22 892 km²'
+        area: '22 892 km²',
+        bgImg: zpBg
     },
     {
         key: 1,
         province: 'Pomorskie',
         capital: 'Gdańsk',
         population: '2 305 000',
-        area: '18 310 km²'
+        area: '18 310 km²',
+        bgImg: pmBg
     },
     {
         key: 2,
         province: 'Warmińsko-Mazurskie',
         capital: 'Olsztyn',
         population: '1 408 000',
-        area: '24 173 km²'
+        area: '24 173 km²',
+        bgImg: wmBg
     },
     {
         key: 3,
         province: 'Podlaskie',
         capital: 'Białystok',
         population: '1 193 000',
-        area: '52 284 km²'
+        area: '52 284 km²',
+        bgImg: pdBg
     },
     {
         key: 4,
         province: 'Lubuskie',
         capital: 'Zielona Góra/Gorzów Wielkopolski',
         population: '1 003 000',
-        area: '13 988 km²'
+        area: '13 988 km²',
+        bgImg: lbBg
     },
     {
         key: 5,
         province: 'Wielkopolskie',
         capital: 'Poznań',
         population: '3 473 000',
-        area: '29 826 km²'
+        area: '29 826 km²',
+        bgImg: wpBg
     },
     {
         key: 6,
         province: 'Kujawsko-Pomorskie',
         capital: 'Bydgoszcz/Toruń',
         population: '2 060 000',
-        area: '17 971 km²'
+        area: '17 971 km²',
+        bgImg: kpBg
     },
     {
         key: 7,
         province: 'Łódzkie',
         capital: 'Łódź',
         population: '2 466 000',
-        area: '18 219 km²'
+        area: '18 219 km²',
+        bgImg: ldBg
     },
     {
         key: 8,
         province: 'Mazowieckie',
         capital: 'Warszawa',
         population: '5 385 000',
-        area: '35 558 km²'
+        area: '35 558 km²',
+        bgImg: mzBg
     },
     {
         key: 9,
         province: 'Lubelskie',
         capital: 'Lublin',
         population: '2 148 000',
-        area: '25 122 km²'
+        area: '25 122 km²',
+        bgImg: luBg
     },
     {
         key: 10,
         province: 'Dolnośląskie',
         capital: 'Wrocław',
         population: '2 865 000',
-        area: '19 947 km²'
+        area: '19 947 km²',
+        bgImg: dsBg
     },
     {
         key: 11,
         province: 'Opolskie',
         capital: 'Opole',
         population: '946 000',
-        area: '9 412 km²'
+        area: '9 412 km²',
+        bgImg: opBg
     },
     {
         key: 12,
         province: 'Śląskie',
         capital: 'Katowice',
         population: '4 489 000',
-        area: '12 333 km²'
+        area: '12 333 km²',
+        bgImg: slBg
     },
     {
         key: 13,
         province: 'Świętokrzyskie',
         capital: 'Kielce',
         population: '1 263 000',
-        area: '11 710 km²'
+        area: '11 710 km²',
+        bgImg: skBg
     },
     {
         key: 14,
         province: 'Małopolskie',
         capital: 'Kraków',
         population: '3 349 000',
-        area: '15 183 km²'
+        area: '15 183 km²',
+        bgImg: maBg
     },
     {
         key: 15,
         province: 'Podkarpackie',
         capital: 'Rzeszów',
         population: '2 129 000',
-        area: '17 846 km²'
+        area: '17 846 km²',
+        bgImg: pkBg
     }
 ]
 
