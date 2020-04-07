e8993620b67046afd0f83c4b048f653372e82570
Jacek Grochowina <jacek99@vp.pl>
2020-04-07 14:36:04 +0200

    init

---
+++ src/data/index.js
@@ -0,0 +1,116 @@
+const DATA = [
+    {
+        key: 0,
+        province: 'Zachodnio-Pomorskie',
+        capital: 'Szczecin',
+        population: '1 679 000',
+        area: '22 892 km²'
+    },
+    {
+        key: 1,
+        province: 'Pomorskie',
+        capital: 'Gdańsk',
+        population: '2 305 000',
+        area: '18 310 km²'
+    },
+    {
+        key: 2,
+        province: 'Warmińsko-Mazurskie',
+        capital: 'Olsztyn',
+        population: '1 408 000',
+        area: '24 173 km²'
+    },
+    {
+        key: 3,
+        province: 'Podlaskie',
+        capital: 'Białystok',
+        population: '1 193 000',
+        area: '52 284 km²'
+    },
+    {
+        key: 4,
+        province: 'Lubuskie',
+        capital: 'Zielona Góra/Gorzów Wielkopolski',
+        population: '1 003 000',
+        area: '13 988 km²'
+    },
+    {
+        key: 5,
+        province: 'Wielkopolskie',
+        capital: 'Poznań',
+        population: '3 473 000',
+        area: '29 826 km²'
+    },
+    {
+        key: 6,
+        province: 'Kujawsko-Pomorskie',
+        capital: 'Bydgoszcz/Toruń',
+        population: '2 060 000',
+        area: '17 971 km²'
+    },
+    {
+        key: 7,
+        province: 'Łódzkie',
+        capital: 'Łódź',
+        population: '2 466 000',
+        area: '18 219 km²'
+    },
+    {
+        key: 8,
+        province: 'Mazowieckie',
+        capital: 'Warszawa',
+        population: '5 385 000',
+        area: '35 558 km²'
+    },
+    {
+        key: 9,
+        province: 'Lubelskie',
+        capital: 'Lublin',
+        population: '2 148 000',
+        area: '25 122 km²'
+    },
+    {
+        key: 10,
+        province: 'Dolnośląskie',
+        capital: 'Wrocław',
+        population: '2 865 000',
+        area: '19 947 km²'
+    },
+    {
+        key: 11,
+        province: 'Opolskie',
+        capital: 'Opole',
+        population: '946 000',
+        area: '9 412 km²'
+    },
+    {
+        key: 12,
+        province: 'Śląskie',
+        capital: 'Katowice',
+        population: '4 489 000',
+        area: '12 333 km²'
+    },
+    {
+        key: 13,
+        province: 'Świętokrzyskie',
+        capital: 'Kielce',
+        population: '1 263 000',
+        area: '11 710 km²'
+    },
+    {
+        key: 14,
+        province: 'Małopolskie',
+        capital: 'Kraków',
+        population: '3 349 000',
+        area: '15 183 km²'
+    },
+    {
+        key: 15,
+        province: 'Podkarpackie',
+        capital: 'Rzeszów',
+        population: '2 129 000',
+        area: '17 846 km²'
+    }
+]
+
+export default DATA;
