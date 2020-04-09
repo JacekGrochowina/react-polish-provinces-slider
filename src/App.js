f3d7ed2b17a2338eb5e0c45fb7ac01ed76098b61
Jacek Grochowina <jacek99@vp.pl>
2020-04-09 13:25:32 +0200

    change background image of active province

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { ReactComponent as Scene } from './assets/poland.svg';
 import { Row, Col } from 'styled-bootstrap-grid';
 
@@ -11,19 +11,51 @@ import Info from './components/Info';
 
 function App() {
 
+  // state of slides (active = true/false)
+  const [slides, setSlides] = useState([
+    true,  // Zachodnio-Pomorskie
+    false,  // Pomorskie
+    false,  // Warmińsko-Mazurskie
+    false,  // Podlaskie
+    false,  // Lubuskie
+    false,  // Wielkopolskie
+    false,  // Kujawsko-Pomorskie
+    false,  // Łódzkie
+    false,   // Mazowieckie
+    false,  // Lubelskie
+    false,  // Dolnośląskie
+    false,  // Opolskie
+    false,  // Śląskie
+    false,  // Świętokrzyskie
+    false,  // Małopolskie
+    false   // Podkarpackie
+  ]);
+
+  // return => (number)active slide
+  const getActiveSlide = () => {
+    let activeSlide = null;
+    let found = false;
+    let counter = 0;
+    while (!found) {
+      if (slides[counter]) found = true;
+      if (found) activeSlide = counter;
+      if (counter >= slides.length) {
+        found = !found;
+        activeSlide = null;
+      } else {
+        counter++;
+      }
+    }
+    return activeSlide;
+  }
+  const [activeSlide, setActiveSlide] = useState(getActiveSlide());
+
+  // ref to provinces svg
   const map = useRef(null);
-
-
-
   useEffect(() => {
     const [elements] = map.current.children;
-
     let provinces = [];
-
-    const pushToArray = (value) => {
-      provinces.push(elements.getElementById(value));
-    }
-
+    const pushToArray = (value) => provinces.push(elements.getElementById(value));
     pushToArray('zachodnio-pomorskie');
     pushToArray('pomorskie');
     pushToArray('warminsko-mazurskie');
@@ -41,35 +73,51 @@ function App() {
     pushToArray('malopolskie');
     pushToArray('podkarpackie');
 
-    provinces.forEach((item, number) => item.addEventListener('click', () => {
-      console.log(`id:            ${item.id}`)
-      console.log(`key:           ${data[number].key}`);
-      console.log(`województwo:   ${data[number].province}`);
-      console.log(`stolica:       ${data[number].capital}`);
-      console.log(`populacja:     ${data[number].population}`);
-      console.log(`powierzchnia:  ${data[number].area}`);
-      console.log(`tło:           ${data[number].bgImg}`);
-      console.log(`------------------------------------`);
-    }))
-  })
+    provinces.forEach((item, number) => {
+      // show active province on start app
+      provinces[activeSlide].setAttribute('active', 'true')
+      // show active province after click
+      item.addEventListener('click', () => {
+        for (let i = 0; i < slides.length; i++) slides[i] = false;
 
+        setSlides([...slides], slides[number] = true)
+        setActiveSlide(getActiveSlide())
+
+        provinces.forEach((item, number) => {
+          if (number === getActiveSlide()) {
+            item.setAttribute('active', 'true')
+          } else {
+            item.setAttribute('active', 'false')
+          }
+        })
+      })
+    })
+  }, [])
+
+  // style of App
   const style = {
     height: '100vh',
-    backgroundColor: '#222',
+    backgroundImage: `url(${data[activeSlide].bgImg})`
   }
 
   return (
     <div className="App" style={style}>
 
-      <Row>
-        <Col col={6} ref={map}>
-          <Scene />
-        </Col>
+      <div className="page-wraper">
+        <Row>
+          <Col col={6} ref={map} style={{
+            width: '100%'
+          }}>
+            <Scene />
+          </Col>
 
-        <Col col={6}>
-          <Info />
-        </Col>
-      </Row>
+          <Col col={6} style={{
+            width: '100%'
+          }}>
+            <Info active={activeSlide} />
+          </Col>
+        </Row>
+      </div>
 
     </div>
   );
