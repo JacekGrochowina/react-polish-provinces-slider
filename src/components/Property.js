f4ef9e5a475c015d2d710b7baa364dae7c644c4b
Jacek Grochowina <jacek99@vp.pl>
2020-04-10 14:22:46 +0200

    add animation gsap

--- src/components/Property.js
+++ src/components/Property.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import gsap from 'gsap';
 
 import './Property.scss';
 
 const Property = (props) => {
+
+    const data = useRef(null);
+    useEffect(() => {
+        gsap.set([data.current], { duration: 1, autoAlpha: 0, y: 30 })
+        gsap.to(data.current, { autoAlpha: 1, delay: 0.35, y: 0 })
+    })
+
     return (
         <div className="info__property property">
             <h2 className="property__title">{props.name}</h2>
-            <h1 className="property__data">{props.value}</h1>
+            <h1 className="property__data" ref={data}>{props.value}</h1>
         </div>
     )
 }
