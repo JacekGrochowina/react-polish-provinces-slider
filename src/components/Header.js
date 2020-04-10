5f4c86124fef324c55c1e2460b8f274999dd89f9
Jacek Grochowina <jacek99@vp.pl>
2020-04-10 14:20:58 +0200

    add animation gsap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import gsap from 'gsap';
 
 import './Header.scss';
 
 const Header = (props) => {
+
+    const title = useRef(null);
+    useEffect(() => {
+        gsap.set([title.current, title.current.child], { duration: 1, autoAlpha: 0, x: -100 })
+        gsap.to(title.current, { autoAlpha: 1, x: 0 })
+    })
+
     return (
-        <header className="info__header header">
+        <header className="info__header header" ref={title}>
             <h1 className="header__title">{props.value}</h1>
         </header>
     )
